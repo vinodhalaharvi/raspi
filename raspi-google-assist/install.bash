4684d23c935f8723668b0f6d85bc67feaeecf7a6
Vinod Halaharvi <vinod.halaharvi@gmail.com>
2017-05-18 02:53:11 +0000

    changes ..

---
+++ raspi-google-assist/install.bash
@@ -0,0 +1,15 @@
+pip install --upgrade google-assistant-sdk
+pip install --upgrade google-auth-oauthlib[tool]
+google-oauthlib-tool --client-secrets raspi-assistant.json --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless
+sudo apt-get install portaudio19-dev libffi-dev libssl-dev
+pip install --upgrade google-assistant-sdk[samples]
+googlesamples-assistant-audiotest --record-time 10
+googlesamples-assistant-audiotest --record-time 10 --audio-block-size=3200 --audio-flush-size=6400
+
+sudo apt-get install portaudio19-dev libffi-dev libssl-dev
+pip install --upgrade google-assistant-sdk[samples]
+googlesamples-assistant-pushtotalk
+
+pip install --upgrade google_assistant_library-0.0.2-py2.py3-none-linux_armv7l.whl
+pip install --upgrade google-assistant-sdk[samples]
+googlesamples-assistant-hotword
