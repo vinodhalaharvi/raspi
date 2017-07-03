b45ffb88154cdd83553a286f63f6d2781450a0cf
Vinod Halaharvi <vinod.halaharvi@gmail.com>
2017-07-03 19:26:12 -0400

    adding google assist setup ..

---
+++ setup-google-assistant.bash
@@ -0,0 +1,5 @@
+pip install --upgrade google-assistant-sdk
+pip install --upgrade google-auth-oauthlib[tool]
+google-oauthlib-tool --client-secrets path/to/client_secret_XXXXX.json --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless
+sudo apt-get install portaudio19-dev libffi-dev libssl-dev
+pip install --upgrade google-assistant-sdk[samples]
